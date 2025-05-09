c6db02eeca364c54e2993787447d61e2e01aa8bc
tsundere-connoisseur <vanodaniel111@gmail.com>
2025-05-09 21:28:18 +0200

    finish the overengineered constructor for Question.cs

--- Game/Question.cs
+++ Game/Question.cs
@@ -1,29 +1,54 @@
 using System.Linq.Expressions;
 using JetBrains.Annotations;
+using LOIM.Util;
 
 namespace LOIM.Game;
 
 public readonly struct Question
 {
-    [PublicAPI] public const    byte                     MaxLevel      = 15;
-    [PublicAPI] public const    byte                     MinLevel      = 1;
-    [PublicAPI] public const    byte                     QuestionCount = 4;
-    [PublicAPI] public readonly byte                     Difficulty;
-    [PublicAPI] public readonly string                   Category;
-    [PublicAPI] public readonly string                   QuestionText;
-    [PublicAPI] public readonly Dictionary<char, string> Answers;
-    [PublicAPI] public readonly char                     CorrectAnswer;
+    [PublicAPI] public const    byte     MaxLevel      = 15;
+    [PublicAPI] public const    byte     MinLevel      = 1;
+    [PublicAPI] public const    byte     QuestionCount = 4;
+    [PublicAPI] public readonly byte     Difficulty;
+    [PublicAPI] public readonly string   Category;
+    [PublicAPI] public readonly string   QuestionText;
+    [PublicAPI] public readonly string[] Answers = new string[QuestionCount];
+    [PublicAPI] public readonly char     CorrectAnswer;
 
+    [PublicAPI]
     public static Question Parse(in string line, char delimiter = ';') => new(in line, delimiter);
+
+    /// <summary>
+    /// returns whether the input character is the correct answer or not
+    /// <param name="guess">value in the A..=D range</param>
+    /// </summary>
+    [PublicAPI]
+    public bool Guess([ValueRange('A', 'D')] char guess) => guess == CorrectAnswer;
 
     private Question(in string repr, char delimiter = ';')
     {
         var src          = repr.AsSpan();
         var dataSegments = src.Split(delimiter);
 
-        EnsureNext(dataSegments);
+        dataSegments.EnsureNext();
         ParseData(ref Difficulty, src[dataSegments.Current], src => byte.Parse(src),
                   res => res >= MinLevel && res <= MaxLevel);
+
+        dataSegments.EnsureNext();
+        QuestionText = src[dataSegments.Current].ToString();
+
+        for (byte i = 0; i < QuestionCount; i++)
+        {
+            dataSegments.EnsureNext();
+            Answers[i] = src[dataSegments.Current].ToString();
+        }
+
+        dataSegments.EnsureNext();
+        ParseData(ref CorrectAnswer, src[dataSegments.Current], src => char.ToUpper(src[0]),
+                  res => char.IsBetween(res, 'A', 'D'));
+
+        dataSegments.EnsureNext();
+        Category = src[dataSegments.Current].ToString();
     }
 
     private static void ParseData<T>(ref T dest, ReadOnlySpan<char> src, Func<ReadOnlySpan<char>, T> parser,
@@ -33,10 +58,5 @@ public readonly struct Question
         var validator = validate.Compile();
         if (!validator(res)) throw new FormatException($"Validation failed for value {res} ({validate.Body})");
         dest = res;
-    }
-
-    private static void EnsureNext(MemoryExtensions.SpanSplitEnumerator<char> enumerator)
-    {
-        if (!enumerator.MoveNext()) throw new FormatException("input string does not contain all of the required data");
     }
 }
